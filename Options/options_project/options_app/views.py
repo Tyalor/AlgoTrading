8db50aeed1303c74d4b86fa537d8996f54e09036
Tyalor <tyalorkny@gmail.com>
2024-01-17 21:05:36 -0500

    deploy v0.0.1

--- Options/options_project/options_app/views.py
+++ Options/options_project/options_app/views.py
@@ -5,54 +5,60 @@ from django.shortcuts import render
 
 
 def index(request):
+    error_message = None  # Variable to store the error message
+
     if request.method == 'POST':
         ticker_input = request.POST.get('tickers')
         start_date = request.POST.get('start_date')
         end_date = request.POST.get('end_date')
-
-        # Safely convert form inputs to float or int, with defaults for empty fields
         min_price = request.POST.get('min_price')
         max_price = request.POST.get('max_price')
-        std_dev_filter = request.POST.get('std_dev_filter')
-        min_volume = request.POST.get('min_volume')
-        min_open_interest = request.POST.get('min_open_interest')
 
+        # Check if required fields are filled
+        if not (ticker_input and start_date and end_date and min_price and max_price):
+            error_message = "Please fill in all required fields marked with an asterisk (*)"
 
-        min_price = float(min_price) if min_price else 0.0
-        max_price = float(max_price) if max_price else 0.0
-        std_dev_filter = float(std_dev_filter) if std_dev_filter else None
-        min_volume = int(min_volume) if min_volume else 0
-        min_open_interest = int(min_open_interest) if min_open_interest else 0
+        if not error_message:
+            # Convert inputs safely to float or int, with defaults for empty fields
+            std_dev_filter = request.POST.get('std_dev_filter')
+            min_volume = request.POST.get('min_volume')
+            min_open_interest = request.POST.get('min_open_interest')
 
-        tickers = [ticker.strip() for ticker in ticker_input.split(',')]
-        dates = generate_date_range(start_date, end_date)
+            min_price = float(min_price) if min_price else 0.0
+            max_price = float(max_price) if max_price else 0.0
+            std_dev_filter = float(std_dev_filter) if std_dev_filter else None
+            min_volume = int(min_volume) if min_volume else 0
+            min_open_interest = int(min_open_interest) if min_open_interest else 0
 
-        processed_data = []
-        for ticker_symbol in tickers:
-            ticker_data = {}
-            ticker = yf.Ticker(ticker_symbol)
-            last_price, timestamp, put_to_call_ratio = get_underlying_data(ticker)
-            ticker_data['symbol'] = ticker_symbol
-            ticker_data['last_price'] = last_price
-            ticker_data['timestamp'] = timestamp
-            ticker_data['put_to_call_ratio'] = put_to_call_ratio
+            tickers = [ticker.strip() for ticker in ticker_input.split(',')]
+            dates = generate_date_range(start_date, end_date)
 
-            ticker_data['options'] = []
-            for expiration_date in dates:
-                calls, puts = get_filtered_options_data(ticker_symbol, expiration_date, min_price, max_price, min_volume, min_open_interest, std_dev_filter)
-                if calls and puts:
-                    ticker_data['options'].append({
-                        'expiration_date': expiration_date,
-                        'calls': calls,
-                        'puts': puts
-                    })
+            processed_data = []
+            for ticker_symbol in tickers:
+                ticker_data = {}
+                ticker = yf.Ticker(ticker_symbol)
+                last_price, timestamp, put_to_call_ratio = get_underlying_data(ticker)
+                ticker_data['symbol'] = ticker_symbol
+                ticker_data['last_price'] = last_price
+                ticker_data['timestamp'] = timestamp
+                ticker_data['put_to_call_ratio'] = put_to_call_ratio
 
-            processed_data.append(ticker_data)
-            print(f"Processed Data: {processed_data}")
+                ticker_data['options'] = []
+                for expiration_date in dates:
+                    calls, puts = get_filtered_options_data(ticker_symbol, expiration_date, min_price, max_price, min_volume, min_open_interest, std_dev_filter)
+                    if calls and puts:
+                        ticker_data['options'].append({
+                            'expiration_date': expiration_date,
+                            'calls': calls,
+                            'puts': puts
+                        })
 
-        return render(request, 'options_app/index.html', {'data': processed_data})
+                processed_data.append(ticker_data)
 
-    return render(request, 'options_app/index.html')
+            return render(request, 'options_app/index.html', {'data': processed_data, 'error_message': error_message})
+
+    # For a GET request or if there's an error message, render the form
+    return render(request, 'options_app/index.html', {'error_message': error_message})
 
 
 # Function to get put-to-call ratio and last price of the underlying
